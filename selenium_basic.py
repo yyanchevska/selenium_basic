94137b16bc21d853fa4e21914e8ce94b61834027
rhonyshniuk <roman.honyshniuk@datarobot.com>
2020-07-22 09:57:41 +0300

    Create selenium_basic.py

---
+++ selenium_basic.py
@@ -0,0 +1,9 @@
+# Import WD lib
+
+# Create WD Chrome Object
+
+# Open "https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html"
+
+# Click on [Normal success message button]
+
+# assert appeared green success message
