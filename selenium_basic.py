9f35e50314de0d8658f2239bed29d351ae1a4fa7
“yyanchevska” <yana.yanchevska10@gmail.com>
2020-07-26 21:47:20 +0300

    My third commit

--- selenium_basic.py
+++ selenium_basic.py
@@ -7,3 +7,18 @@
 # Click on [Normal success message button]
 
 # assert appeared green success message
+
+from selenium import webdriver
+import time
+driver = webdriver.Chrome()
+
+driver.get("https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html")
+time.sleep(3)
+
+driver.find_element_by_id('normal-btn-success').click()
+time.sleep(3)
+
+assert "I'm a normal success message." in driver.page_source
+time.sleep(3)
+
+driver.quit()
